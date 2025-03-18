5238b53ea02b2d0bfb93ab4830734cfedaecfba8
Pravallika-Narla <narlapravallika13@gmail.com>
2025-03-18 13:13:03 +0530

    hello

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,7 +2,7 @@ import { signInWithEmailAndPassword } from "firebase/auth";
 import React, { useState } from "react";
 import { auth } from "./firebase";
 import { toast } from "react-toastify";
-import SignInwithGoogle from "./signInWIthGoogle";
+import SignInwithGoogle from "./SignInwithGoogle";
 import {useNavigate} from 'react-router-dom'
 
 function Login() {
